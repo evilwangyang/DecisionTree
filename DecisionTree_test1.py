9a19153f1bb712dffe2b77c20db7af9313a8b194
evilwangyang <evilwangyang@126.com>
2018-07-25 22:55:16 +0800

    在DecisionTree_test1中增加createDataSet函数

--- DecisionTree_test1.py
+++ DecisionTree_test1.py
@@ -24,3 +24,17 @@ def calcShannonEnt(dataSet):
 		prob = float(labelCounts[key])/numEntries
 		shannonEnt -= prob * log(prob,2)
 	return shannonEnt
+
+def createDataSet():
+	dataSet = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+	labels = ['no surfacing','flippers']
+	return dataSet,labels
+
+if __name__ == '__main__':
+
+	myDat,labels = createDataSet()
+	shannonEnt1 = calcShannonEnt(myDat)
+	print(shannonEnt1)
+	myDat[0][-1] = 'maybe'
+	shannonEnt2 = calcShannonEnt(myDat)
+	print(shannonEnt2)
