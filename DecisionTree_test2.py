6f123c329bab907ffc6dde0a3a98caf935ef30b3
evilwangyang <evilwangyang@126.com>
2018-07-27 16:34:57 +0800

    完成了DecisionTree_test1，新建了DecisionTree_test2

---
+++ DecisionTree_test2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+# -*- coding:utf-8 -*-
+
+# @Time      :  2018/7/27 16:33
+# @Auther    :  WangYang
+# @Email     :  evilwangyang@126.com
+# @Project   :  DecisionTree
+# @File      :  DecisionTree_test2.py
+# @Software  :  PyCharm
+
+# ********************************************************* 
