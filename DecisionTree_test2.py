d1b589f210a80e33ee2f11ef96991a0a031a0fac
evilwangyang <evilwangyang@126.com>
2018-07-30 00:00:29 +0800

    在DecisionTree_test2中增加getNumLeafs函数、getTreeDepth函数

--- DecisionTree_test2.py
+++ DecisionTree_test2.py
@@ -29,5 +29,29 @@ def createPlot():
 	plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 	plt.show()
 
+def getNumLeafs(myTree):
+	numLeafs = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			numLeafs += getNumLeafs(secondDict[key])
+		else:
+			numLeafs += 1
+	return numLeafs
+
+def getTreeDepth(myTree):
+	maxDepth = 0
+	firstStr = myTree.keys()[0]
+	secondDict = myTree[firstStr]
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			thisDepth = 1 + getTreeDepth(secondDict[key])
+		else:
+			thisDepth = 1
+		if thisDepth > maxDepth:
+			maxDepth = thisDepth
+	return maxDepth
+
 if __name__ == '__main__':
 	createPlot()
