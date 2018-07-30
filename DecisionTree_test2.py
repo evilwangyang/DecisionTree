af057b5f66d001683e55722fdacddaf7ad7f4bbd
evilwangyang <evilwangyang@126.com>
2018-07-30 23:08:21 +0800

    在DecisionTree_test2中增加plotMidText函数、createPlot函数，更新plotTree函数

--- DecisionTree_test2.py
+++ DecisionTree_test2.py
@@ -19,19 +19,19 @@ def plotNode(nodeTxt,centerPt,parentPt,nodeType):
 	createPlot.ax1.annotate(nodeTxt,xy=parentPt,xycoords='axes fraction',xytext=centerPt,textcoords='axes fraction',
 	                        va="center",ha="center",bbox=nodeType,arrowprops=arrow_args)
 
-def createPlot():
-	fig = plt.figure(1,facecolor='white')
-	fig.clf()
-	createPlot.ax1 =plt.subplot(111,frameon=False)
-	plotNode('决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
-	plotNode('叶节点',(0.8,0.1),(0.3,0.8),leafNode)
-	plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
-	plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
-	plt.show()
+# def createPlot():
+# 	fig = plt.figure(1,facecolor='white')
+# 	fig.clf()
+# 	createPlot.ax1 =plt.subplot(111,frameon=False)
+# 	plotNode('决策节点',(0.5,0.1),(0.1,0.5),decisionNode)
+# 	plotNode('叶节点',(0.8,0.1),(0.3,0.8),leafNode)
+# 	plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+# 	plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+# 	plt.show()
 
 def getNumLeafs(myTree):
 	numLeafs = 0
-	firstStr = myTree.keys()[0]
+	firstStr = list(myTree.keys())[0]
 	secondDict = myTree[firstStr]
 	for key in secondDict.keys():
 		if type(secondDict[key]).__name__=='dict':
@@ -42,7 +42,7 @@ def getNumLeafs(myTree):
 
 def getTreeDepth(myTree):
 	maxDepth = 0
-	firstStr = myTree.keys()[0]
+	firstStr = list(myTree.keys())[0]
 	secondDict = myTree[firstStr]
 	for key in secondDict.keys():
 		if type(secondDict[key]).__name__=='dict':
@@ -53,5 +53,56 @@ def getTreeDepth(myTree):
 			maxDepth = thisDepth
 	return maxDepth
 
+def retrieveTree(i):
+	listOfTrees = [{'no surfacing':{0:'no',1:{'flippers':{0:'no',1:'yes'}}}},{'no surfacing':{0:'no',1:
+		{'flippers':{0:{'head':{0:'no',1:'yes'}},1:'no'}}}}]
+	return listOfTrees[i]
+
+def plotMidText(cntrPt,parentPt,txtString):
+	xMid = (parentPt[0]-cntrPt[0])/2.0 + cntrPt[0]
+	yMid = (parentPt[1]-cntrPt[1])/2.0 + cntrPt[1]
+	createPlot.ax1.text(xMid,yMid,txtString)
+
+def plotTree(myTree,parentPt,nodeTxt):
+	numLeafs = getNumLeafs(myTree)
+	depth = getTreeDepth(myTree)
+	firstStr = list(myTree.keys())[0]
+	cntrPt = (plotTree.xOff + (1.0 + float(numLeafs))/2.0/plotTree.totalW,plotTree.yOff)
+	plotMidText(cntrPt,parentPt,nodeTxt)
+	plotNode(firstStr,cntrPt,parentPt,decisionNode)
+	secondDict = myTree[firstStr]
+	plotTree.yOff = plotTree.yOff - 1.0/plotTree.totalD
+	for key in secondDict.keys():
+		if type(secondDict[key]).__name__=='dict':
+			plotTree(secondDict[key],cntrPt,str(key))
+		else:
+			plotTree.xOff = plotTree.xOff + 1.0/plotTree.totalW
+			plotNode(secondDict[key],(plotTree.xOff,plotTree.yOff),cntrPt,leafNode)
+			plotMidText((plotTree.xOff,plotTree.yOff),cntrPt,str(key))
+	plotTree.yOff = plotTree.yOff + 1.0/plotTree.totalD
+
+def createPlot(inTree):
+	fig = plt.figure(1,facecolor='white')
+	fig.clf()
+	axprops = dict(xticks=[],yticks=[])
+	createPlot.ax1 = plt.subplot(111,frameon=False,**axprops)
+	plotTree.totalW = float(getNumLeafs(inTree))
+	plotTree.totalD = float(getTreeDepth(inTree))
+	plotTree.xOff = -0.5/plotTree.totalW
+	plotTree.yOff = 1.0
+	plotTree(inTree,(0.5,1.0),'')
+	plt.rcParams['font.sans-serif'] = ['SimHei']  # 用来正常显示中文标签
+	plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+	plt.show()
+
+
 if __name__ == '__main__':
-	createPlot()
+	# createPlot()
+
+	Tree1 = retrieveTree(1)
+	print(Tree1)
+	myTree = retrieveTree(0)
+	Leafs = getNumLeafs(myTree)
+	print(Leafs)
+	Depth = getTreeDepth(myTree)
+	print(Depth)
