d734b632f0f157dc2dea7194d9902c259fefb890
evilwangyang <evilwangyang@126.com>
2018-07-31 17:27:16 +0800

    将DecisionTree_test1中的函数添加到DecisionTree_test2中

--- DecisionTree_test2.py
+++ DecisionTree_test2.py
@@ -10,6 +10,81 @@
 
 # ********************************************************* 
 import matplotlib.pyplot as plt
+from math import log
+import operator
+
+def calcShannonEnt(dataSet):
+	numEntries = len(dataSet)
+	labelCounts = {}
+	for featVec in dataSet:
+		currentLabel = featVec[-1]
+		if currentLabel not in labelCounts.keys():
+			labelCounts[currentLabel] = 0
+		labelCounts[currentLabel] += 1
+	shannonEnt = 0.0
+	for key in labelCounts:
+		prob = float(labelCounts[key])/numEntries
+		shannonEnt -= prob * log(prob,2)
+	return shannonEnt
+
+def createDataSet():
+	dataSet = [[1,1,'yes'],[1,1,'yes'],[1,0,'no'],[0,1,'no'],[0,1,'no']]
+	labels = ['no surfacing','flippers']
+	return dataSet,labels
+
+def splitDataSet(dataSet,axis,value):
+	retDataSet = []
+	for featVec in dataSet:
+		if featVec[axis] == value:
+			reducedFeatVec = featVec[:axis]
+			reducedFeatVec.extend(featVec[axis+1:])
+			retDataSet.append(reducedFeatVec)
+	return retDataSet
+
+def chooseBestFeatureToSplit(dataSet):
+	numFeatures = len(dataSet[0]) - 1
+	baseEntropy = calcShannonEnt(dataSet)
+	bestInfoGain = 0.0
+	bestFeature = -1
+	for i in range(numFeatures):
+		featList = [example[i] for example in dataSet]
+		uniqueVals = set(featList)
+		newEntropy = 0.0
+		for value in uniqueVals:
+			subDataSet = splitDataSet(dataSet,i,value)
+			prob = len(subDataSet)/float(len(dataSet))
+			newEntropy += prob * calcShannonEnt(subDataSet)
+		infoGain = baseEntropy - newEntropy
+		if infoGain > bestInfoGain:
+			bestInfoGain = infoGain
+			bestFeature = i
+	return bestFeature
+
+def majorityCnt(classList):
+	classCount = {}
+	for vote in classList:
+		if vote not in classCount.keys():
+			classCount[vote] = 0
+		classCount[vote] += 1
+	sortedClassCount = sorted(classCount.items(),key=operator.itemgetter(1),reverse=True)
+	return  sortedClassCount[0][0]
+
+def createTree(dataSet,labels):
+	classList = [example[-1] for example in dataSet]
+	if classList.count(classList[0]) == len(classList):
+		return classList[0]
+	if len(dataSet[0]) == 1:
+		return majorityCnt(classList)
+	bestFeat = chooseBestFeatureToSplit(dataSet)
+	bestFeatLabel = labels[bestFeat]
+	myTree = {bestFeatLabel:{}}
+	del(labels[bestFeat])
+	featValues = [example[bestFeat] for example in dataSet]
+	uniqueVals = set(featValues)
+	for value in uniqueVals:
+		subLabels = labels[:]
+		myTree[bestFeatLabel][value] = createTree(splitDataSet(dataSet,bestFeat,value),subLabels)
+	return myTree
 
 decisionNode = dict(boxstyle="sawtooth",fc="0.8")
 leafNode = dict(boxstyle="round4",fc="0.8")
@@ -99,10 +174,15 @@ def createPlot(inTree):
 if __name__ == '__main__':
 	# createPlot()
 
-	Tree1 = retrieveTree(1)
-	print(Tree1)
-	myTree = retrieveTree(0)
-	Leafs = getNumLeafs(myTree)
-	print(Leafs)
-	Depth = getTreeDepth(myTree)
-	print(Depth)
+	# Tree1 = retrieveTree(1)
+	# print(Tree1)
+	# myTree = retrieveTree(0)
+	# Leafs = getNumLeafs(myTree)
+	# print(Leafs)
+	# Depth = getTreeDepth(myTree)
+	# print(Depth)
+
+	dataSet,labels = createDataSet()
+	myTree = createTree(dataSet,labels)
+	print(myTree)
+	createPlot(myTree)
